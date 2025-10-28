81fad0f63bb223ec2e756260fa739408a12c0a64
Miguel Sousa <misousa@c1r7s8.42lisboa.com>
2025-10-28 15:18:25 +0000

    fixed trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,60 +6,57 @@
 /*   By: misousa <misousa@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 14:02:30 by misousa           #+#    #+#             */
-/*   Updated: 2025/10/27 19:54:22 by misousa          ###   ########.fr       */
+/*   Updated: 2025/10/28 15:18:09 by misousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* int	ft_strlen(const char *str)
+static int	checkset(char *set, char c)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (set[i] != '\0')
+	{
+		if (c == set[i])
+			return (1);
 		i++;
-	return (i);
-} */
+	}
+	return (0);
+}
 
 char	*ft_strtrim(char const *s1, char *set)
 {
 	char	*result;
 	int		i;
-	int		j;
-	int		k;
+	int		start;
+	int		end;
 
 	i = 0;
-	j = 0;
-	result = malloc(ft_strlen(s1) - ft_strlen(set) + 1);
-	while (s1[i] != '\0')
-	{
-		k = 0;
-		while (set[k] == '\0')
-		{
-			if (set[k] == s1[i])
-				break ;
-			k++;
-		}
-		if (set[k] != s1[i])
-		{
-			result[j] = s1[i];
-			j++;
-		}
+	end = ft_strlen(s1);
+	while (checkset(set, s1[i]) == 1)
 		i++;
-	}
+	start = i;
+	while (checkset(set, s1[end - 1]) == 1)
+		end--;
+	result = malloc(end - start + 1);
+	i = 0;
+	while (start < end)
+		result[i++] = s1[start++];
+	result[i] = '\0';
 	return (result);
 }
 
-int	main(void)
+/* int	main(void)
 {
 	char *s1;
 	char *set;
 	char *result;
 
-	s1 = "-|bcdedefghijab|-";
+	s1 = "-|-|-|bcdede|fghijab|-";
 	set = "-|";
 	result = ft_strtrim(s1, set);
 
 	printf("%s\n", result);
-}
+} */
