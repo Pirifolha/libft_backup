ff39be3656f2cfb69562a675a2220b451b04cd85
Miguel Sousa <miguelsousa218@gmail.com>
2025-10-29 14:33:40 +0000

    added split

--- ft_split.c
+++ ft_split.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: misousa <misousa@student.42lisboa.com>     +#+  +:+       +#+        */
+/*   By: miguelsousa <miguelsousa@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 17:54:19 by misousa           #+#    #+#             */
-/*   Updated: 2025/10/28 16:57:36 by misousa          ###   ########.fr       */
+/*   Updated: 2025/10/29 14:32:26 by miguelsousa      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_words(char *s, char c)
+static int	count_words(const char *s, char c)
 {
 	int	i;
 	int	words;
@@ -29,7 +29,8 @@ static int	count_words(char *s, char c)
 		words++;
 	return (words);
 }
-static int	*wordlen(char *s, char c, int words)
+
+static int	*wordlen(const char *s, char c, int words)
 {
 	int	i;
 	int	*result;
@@ -55,32 +56,68 @@ static int	*wordlen(char *s, char c, int words)
 	return (result);
 }
 
-/* char	**ft_split(char const *s, char c)
+static char	**writestr(char **result, char const *s, char c, int *len)
+{
+	int	i;
+	int	j;
+	int	k;
+	int	words;
+
+	i = 0;
+	k = 0;
+	words = count_words(s, c);
+	while (i < words)
+	{
+		j = 0;
+		while (j < len[i])
+		{
+			while (s[k] == c)
+				k++;
+			result[i][j] = s[k];
+			j++;
+			k++;
+		}
+		result[i][++j] = '\0';
+		i++;
+	}
+	return (result);
+}
+
+char	**ft_split(char const *s, char c)
 {
 	char	**result;
 	int		words;
+	int		*len;
+	int		i;
 
+	i = 0;
 	words = count_words(s, c);
+	len = wordlen(s, c, words);
+	result = malloc(words * sizeof(char *));
+	while (i < words)
+	{
+		result[i] = malloc(len[i] + 1 * sizeof(char));
+		i++;
+	}
+	result = writestr(result, s, c, len);
+	return (result);
+}
 
-	wordlen(s, c, words);
-
-	return
-} */
-
-int	main(void)
+/* int	main(void)
 {
-	int *res;
-	char *s;
+	char **res;
+	const char *s;
 	char c;
 	int words;
 
-	s = "----sdkas--ggdfvvfd-regerger--fdggfdfgd------asdd325as=--";
+	s = "sdk-==as--ggdfvvfd-regerger--fdggfdfgd------asdd325as=--";
 	c = '-';
 	words = count_words(s, c);
-	res = wordlen(s, c, words);
+	res = ft_split(s, c);
 
 	for (int i = 0; i < words; i++)
 	{
-		printf("%d\n", res[i]);
+		printf("%s\n", res[i]);
 	}
-}
+	free((void *)res);
+} */
